9ba370d0d6e1e9325eaa8a86eee1432bca2fb612
MESSIE MATALA <messiematala77@gmail.com>
2026-02-28 17:11:10 +0100

    Fix init admin

--- SQL/SQL/init_data.sql
+++ SQL/SQL/init_data.sql
@@ -1,1 +1,23 @@
--- Données initiales (admin, rôles)
+IF NOT EXISTS (SELECT 1 FROM dbo.Employes WHERE NomUtilisateur = 'admin')
+BEGIN
+    INSERT INTO dbo.Employes
+    (
+        Nom,
+        Prenom,
+        Poste,
+        NomUtilisateur,
+        MotDePasse,
+        IsManager,
+        IsActif
+    )
+    VALUES
+    (
+        'MATALA',
+        'MESSIE',
+        'Superviseur',
+        'MESSIE',
+        '1234',
+        1,
+        1
+    )
+END-- Données initiales (admin, rôles)
